348391291fba2dd797630a0bab137abc7b94d5d0
Krishan17212 <73638230+Krishan17212@users.noreply.github.com>
2023-12-08 14:46:08 +0530

    updation for mobile view

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -13,16 +13,18 @@ import {
 import { db } from "../db/firebase";
 
 const style = {
-  main: `todo_wrapper relative max-w-4xl w-full bg-zinc-700 p-10 rounded-xl`,
+  main: `todo_wrapper relative max-w-4xl w-full bg-zinc-700 py-10 px-6 lg:p-10 rounded-xl`,
   form: `form_main flex items-center`,
-  formInput: `flex-grow p-3 rounded-md text-xl outline-none`,
-  formBtn: `text-xl`,
-  listItems: `list_items gap-4 flex flex-wrap mt-10`,
+  formInput: `flex-grow p-2 lg:p-3 rounded-md text-md lg:text-xl outline-none`,
+  formBtn: `text-orange sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl`,
+  listItems: `list_items gap-4 flex flex-wrap mt-12`,
 };
 
 const Todo = () => {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
+
+  const iconSize = window.innerWidth > 640 ? 70 : 50;
 
   // create todo
   const createTodo = async (e) => {
@@ -66,7 +68,7 @@ const Todo = () => {
   return (
     <>
       <div className={style.main}>
-        <div className="todo_title text-center font-semibold text-zinc-300 text-6xl mb-8">
+        <div className="todo_title text-center font-semibold text-zinc-300 text-4xl lg:text-6xl mb-8">
           <h1>Todo App</h1>
         </div>
         <div className="todo_form">
@@ -79,7 +81,7 @@ const Todo = () => {
               value={input}
             />
             <button className={style.formBtn}>
-              <MdAddBox size={70} color="orange" />
+              <MdAddBox size={iconSize} color="orange" />
             </button>
           </form>
         </div>
